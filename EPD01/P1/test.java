2afcc3a6a38c80ad36dce9d7bebf0a5132b92d30
Juan Rodriguez <jrodlop5@alu.upo.es>
2024-10-10 00:54:28 +0200

    Comiteada 'el metodo saludar()'

---
+++ EPD01/P1/test.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        saludar();
+    }
+
+    public static void saludar() {
+        System.out.println("Hola, mundo!");
+    }
+
+}
