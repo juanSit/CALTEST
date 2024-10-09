ef725ab886014a320cfdbaa6e73b241d1ab59eb0
Juan Rodriguez <jrodlop5@alu.upo.es>
2024-10-10 01:22:44 +0200

    Comiteado metodo 'saludaAlaClase()'

--- EPD01/P1/test.java
+++ EPD01/P1/test.java
@@ -6,5 +6,9 @@ public class Main {
     public static void saludar() {
         System.out.println("Hola, mundo!");
     }
+	
+    public static void saludoAlaClase() {
+        System.out.println("Hola clase de EPD 1");
+    }
 
 }
